1b942471311f26d986b2275978bbe612a973a102
scott <scblechman@gmail.com>
2019-06-11 00:40:09 -0500

    Alarm activates at specified date and time

--- app/src/main/java/com/scottblechman/otter/lifecycle/BroadcastRepository.java
+++ app/src/main/java/com/scottblechman/otter/lifecycle/BroadcastRepository.java
@@ -3,13 +3,12 @@ package com.scottblechman.otter.lifecycle;
 import android.app.AlarmManager;
 import android.app.Application;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.scottblechman.otter.db.Alarm;
 
 import java.util.Calendar;
+import java.util.Date;
 
 import static android.content.Context.ALARM_SERVICE;
 
@@ -24,8 +23,11 @@ class BroadcastRepository {
     void insert(Alarm alarm) {
         // get a Calendar object with current time
         Calendar cal = Calendar.getInstance();
-        // add 30 seconds to the calendar object
-        cal.add(Calendar.SECOND, 30);
+        // set date and time to alarm values
+        Date date = alarm.getDate();
+
+        cal.set(date.getYear(), date.getMonth(), date.getDate(),
+                date.getHours(), date.getMinutes(), 0);
         Intent intent = new Intent(mApplication, AlarmBroadcastReceiver.class);
         PendingIntent sender = PendingIntent.getBroadcast(mApplication, 192837, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
