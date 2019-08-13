7dd603ee6f8e74ecf1bbb0fbc3b7e0e46e5a9765
scott <scblechman@gmail.com>
2019-08-12 22:06:28 -0500

    Fixed snooze not working on action

--- app/src/main/java/com/scottblechman/otter/services/ActionReceiver.java
+++ app/src/main/java/com/scottblechman/otter/services/ActionReceiver.java
@@ -3,6 +3,7 @@ package com.scottblechman.otter.services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.scottblechman.otter.Otter;
@@ -17,17 +18,24 @@ import org.joda.time.DateTime;
  */
 public class ActionReceiver extends BroadcastReceiver {
 
+    private int mUid;
     private String mLabel;
     private DateTime mDateTime;
+    private boolean mEnabled;
+
+    private static String TAG = ActionReceiver.class.toString();
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        mUid = intent.getIntExtra("uid", -1);
         mLabel = intent.getStringExtra("label");
         mDateTime = new DateTime(intent.getLongExtra("time", new DateTime().getMillis()));
+        mEnabled = intent.getBooleanExtra("enabled", false);
 
         String action = intent.getStringExtra("action");
 
+        Log.d(TAG, "onReceive: performing action " + action + " on " + intent.getExtras().toString());
         if(action.equals("snooze")){
             snoozeAlarm(context);
         }
@@ -43,6 +51,9 @@ public class ActionReceiver extends BroadcastReceiver {
     public void snoozeAlarm(Context context){
         Toast.makeText(context, R.string.alarm_snoozed, Toast.LENGTH_SHORT).show();
         Alarm oldAlarm = new Alarm(mDateTime, mLabel);
+        oldAlarm.setUid(mUid);
+        oldAlarm.setEnabled(mEnabled);
+
         Alarm newAlarm = createSnoozedAlarm(context, oldAlarm);
         BroadcastRepository broadcastRepository = BroadcastRepository.getInstance();
         broadcastRepository.update((Otter)context.getApplicationContext(), oldAlarm, newAlarm);
@@ -58,8 +69,12 @@ public class ActionReceiver extends BroadcastReceiver {
      * @return alarm with a new activation time
      */
     private Alarm createSnoozedAlarm(Context context, Alarm originalAlarm) {
+        Alarm newAlarm = originalAlarm;
+
         int minutes = context.getResources().getInteger(R.integer.snooze_minutes);
         DateTime dateTime = DateTime.now().plusMinutes(minutes);
-        return new Alarm(dateTime, originalAlarm.getLabel());
+
+        newAlarm.setDate(dateTime);
+        return newAlarm;
     }
 }
